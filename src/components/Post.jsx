d13a7468b2093c5c2790c4e63ecaf91d84026b01
James Watson <jameywatson134@gmail.com>
2023-07-15 12:58:36 -0700

    initial

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div className='container'>
+            <div className='poster'>
+                <p className='monsters'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi veniam molestias, sunt debitis nostrum quae reiciendis, sed dolor pariatur accusantium consequatur nam exercitationem cumque dolorum, obcaecati officia dolorem repellendus asperiores?</p>
+            </div>
+        </div>
+    )
+}
+
+export default Post
